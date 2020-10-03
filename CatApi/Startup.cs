5def88ceb2f974f9649e2f27acaf51c97af6d31c
Reisclef <9120292+Reisclef@users.noreply.github.com>
2020-10-03 22:55:15 +0100

    #6 Added Swagger Documentation

--- CatApi/Startup.cs
+++ CatApi/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 namespace CatApi
@@ -41,6 +42,33 @@ namespace CatApi
                     Task.FromResult(
                         credentials.username.StartsWith("user")
                         && credentials.password.EndsWith("word")));
+
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "basic",
+                    In = ParameterLocation.Header,
+                    Description = "Basic Authorization header."
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "basic"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +81,11 @@ namespace CatApi
 
             app.UsePathBase(Configuration["BasePath"] ?? "/");
             app.UseAuthentication();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Cat API");
+            });
 
             app.UseMvc();
         }
