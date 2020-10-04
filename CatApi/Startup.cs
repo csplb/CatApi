8ad53d9186c239b9c75bc0666b559b5daf027a56
jumpmanda <amandajhernandez@outlook.com>
2020-10-04 00:29:01 -0700

    Rename action, remove build error test

--- CatApi/Startup.cs
+++ CatApi/Startup.cs
@@ -43,8 +43,6 @@ namespace CatApi
                         && credentials.password.EndsWith("word")));
         }
 
-        testingbuilderror
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
