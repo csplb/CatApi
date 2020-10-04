d6b0ed2f6bd77b93b6c5589d2b4fbfdaaed57b25
jumpmanda <amandajhernandez@outlook.com>
2020-10-04 00:22:08 -0700

    Update action , testing with build error

--- CatApi/Startup.cs
+++ CatApi/Startup.cs
@@ -43,6 +43,8 @@ namespace CatApi
                         && credentials.password.EndsWith("word")));
         }
 
+        testingbuilderror
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
