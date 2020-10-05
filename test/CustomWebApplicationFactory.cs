6ba21b980523268883b16ae86fff496888b8e328
Marcin Badurowicz <m@badurowicz.net>
2020-10-05 19:33:11 +0200

    Add first set of integration tests

---
+++ test/CustomWebApplicationFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+
+namespace CatApi.Test
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Startup>
+    {
+        protected override IHostBuilder CreateHostBuilder()
+        {
+            return base.CreateHostBuilder();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                // there are no additional actions, but I leave this here for reference
+            });
+        }
+    }
+}
