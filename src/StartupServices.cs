afade691c717fad95e056b5e4174017dcd08f21c
Mati <mwiczuk99@gmail.com>
2020-10-11 15:36:11 +0200

    Add extension methods to IServiceCollection

---
+++ src/StartupServices.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using Bazinga.AspNetCore.Authentication.Basic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace CatApi
+{
+    public static class StartupServices
+    {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "basic",
+                    In = ParameterLocation.Header,
+                    Description = "Basic Authorization header."
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "basic"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+        }
+
+        public static void AddBasicAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthentication(BasicAuthenticationDefaults.AuthenticationScheme)
+                .AddBasicAuthentication(credentials =>
+                    Task.FromResult(
+                        credentials.username.StartsWith("user") 
+                        && credentials.password.EndsWith("word")));
+        }
+
+        public static void ConfigureControllers(this IServiceCollection services)
+        {
+            services.AddControllers().AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.WriteIndented = true;
+                opt.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            });
+        }
+    }
+}
