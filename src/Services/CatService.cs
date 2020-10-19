d80051dc3841b7e75beb9d88222b8829fd3df7e0
Mati <mwiczuk99@gmail.com>
2020-10-19 22:54:28 +0200

    Add CatService

---
+++ src/Services/CatService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CatApi.Models;
+using CatApi.Services.Interfaces;
+
+namespace CatApi.Services
+{
+    public class CatService : ICatService
+    {
+        private readonly CatDbContext _catDbContext;
+
+        public CatService(CatDbContext catDbContext)
+        {
+            _catDbContext = catDbContext;
+            SeedDatabase();
+        }
+
+        public List<Cat> Cats()
+        {
+            return _catDbContext.Cats.ToList();
+        }
+        private void SeedDatabase()
+        {
+            if (_catDbContext.Cats.Any())
+            {
+                return;
+            }
+
+            var cats = GetCatsFromJson();
+
+            if (!cats.Any())
+            {
+                return;
+            }
+            _catDbContext.Cats.AddRange(cats);
+            _catDbContext.SaveChanges();
+        }
+
+        private List<Cat> GetCatsFromJson()
+        {
+            using (FileStream fs = new FileStream("cats.json", FileMode.Open))
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                var file = sr.ReadToEnd();
+                return JsonSerializer.Deserialize<List<Cat>>(file);
+            }
+        }
+    }
+}
---
+++ src/Services/Interfaces/ICatService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CatApi.Models;
+
+namespace CatApi.Services.Interfaces
+{
+    public interface ICatService
+    {
+        List<Cat> Cats();
+    }
+}
